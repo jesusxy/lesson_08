315eb26266b409f0ff7bbea2acf668f0b799c612
Adrian Sandoval <56141563+asandoval2313@users.noreply.github.com>
2022-10-02 21:16:08 -0700

    Added Adrian's function calls

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
 const testAddr = "0xca06eF3aE1E9902A1d5a234c84B5b98b72D22ceA"
+const CHAIRMAN_ADDRESS = "0xaDD95228501c0769b1047975faf93FC798C4E76C";
+const DELEGATE_ADDRESS = "0x7717Bd9b2eF9bE48968a19728c8D71789634728C";
 
 function convertStringArrayToBytes32(array: string[]) {
     const bytes32Array = [];
@@ -35,6 +37,23 @@ async function main() {
     // voter struct testAddr
     const voterStructAddr2 = await ballotContract.voters(signer.address);
     console.log('Voter struct Test Addr: ', {voterStructAddr2});
+    
+    // ADRIAN'S CODE
+    let voterStructForDelegate = await ballotContract.voters(DELEGATE_ADDRESS);
+    console.log({voterStructForDelegate});
+
+    const giveRightToVoteTxAdrian = await ballotContract.giveRightToVote(DELEGATE_ADDRESS);
+    const giveRightToVoteTxAdrianReceipt = await giveRightToVoteTx.wait();
+    console.log({giveRightToVoteTxReceipt});
+
+    const delegateVoteTx = await ballotContract.delegate(DELEGATE_ADDRESS);
+    const delegateVoteTxReceipt = await delegateVoteTx.wait();
+    console.log({delegateVoteTxReceipt});
+
+    voterStructForDelegate = await ballotContract.voters(DELEGATE_ADDRESS);
+    console.log({voterStructForDelegate});
+    
+    
     
 }
 
